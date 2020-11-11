9a30384fa624eb45f07a6fe074afd162a18f3b06
Yordanov <iliyanyyordanov@gmail.com>
2020-11-11 10:21:06 +0000

    Refactor App.js with one of the chart components
    Problem
    I'm considering sell I use the chart.js any more
    
    Solution
    Remove chart.js from App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-import Chart from './chart';
 import QChart from './quickChart';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <QChart title="New chart"  type="line" labels={["Mon", "Thu", "Wed", "Thur", "Fri", "Sat", "Sun"]} data={[5,2,3,4,5,10,1]} animation="zoomIn"  />
-        <Chart />
-        <Chart type="line" title="2020 - COVID-19 cases" data={[
-          12,11,10,9,8,7,6,5,4,3,2,1
-        ]} />
-        <Chart type="pie" title="Home expenses" data={[
-          2000, 5000, 1000, 2000, 4000, 2000, 10000, 13000, 4000, 1000, 3000, 2000
-        ]}  />
-      </header>
+        <h1>
+          Default quick chart component
+        </h1>
+        <QChart />
+        <h1>
+          Quich chart component with specific
+        </h1>
+        <QChart 
+        type="bar" 
+        title="Hours sleep" 
+        labels={["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"]} 
+        data={[8, 7.6, 8, 6, 6, 5, 9, 10]} 
+        animation="zoomIn" 
+        animationDuration="1.4s" />
     </div>
   );
 }
